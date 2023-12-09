cc713ad47bcd7226952dcd9534c216b3fc80f48d
Enes Sapan <115135140+Feel02@users.noreply.github.com>
2023-12-09 23:17:18 +0300

    Update Maze.java

--- Maze.java
+++ Maze.java
@@ -3,10 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
-/**
- *
- * @author 4hm3t
- */
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
